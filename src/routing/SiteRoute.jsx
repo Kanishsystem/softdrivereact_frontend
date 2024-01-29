a618b0495f6084252bfd013e33db2b4cb546b476
Raju10136 <mythri.raja@gmail.com>
2024-01-29 06:58:45 +0530

    ok updated code

--- src/routing/SiteRoute.jsx
+++ src/routing/SiteRoute.jsx
@@ -6,28 +6,19 @@ import MainLayout from "../themes/MainLayout";
 import LoginLayout from "../themes/LoginLayout";
 import PrivateRoute from "./PrivateRoute";
 import { ToastContainer } from "react-toastify";
-import Pdftable from "../pages/Table/Pdftable";
-import Pdfform from "../pages/Table/Pdfform";
-import PdfCalendar from "../pages/Table/PdfCalander";
 import Signup from "../pages/Login/Signup";
 import SmartLogin from "../pages/Login/SmartLogin";
 import TextBoxExample from "../pages/examples/TextBoxExample";
 import TableExample from "../pages/examples/TableExample";
 import SelectBoxExample from "../pages/examples/SelectBoxExample";
 import ModalExample from "../pages/examples/ModalExample";
-import EmailSettings from "../pages/Settings/EmailSettings";
-import FileSettings from "../pages/Settings/FileSettings";
-import LoginSettings from "../pages/Settings/LoginSettings";
-import RemoteServerSettings from "../pages/Settings/RemoteServerSettings";
-import SiteSettings from "../pages/Settings/SiteSettings";
 import SmartDriveLogin from "../pages/Login/SmartDriveLogin";
 import AllFiles from "../pages/AllFiles/AllFiles";
 import ShareWithMe from "../pages/ShareFiles/ShareWithMe";
 import RecyleBin from "../pages/AllFiles/RecyleBin";
 import RoleTable from "../pages/Role/RoleTable";
-import RoleForm from "../pages/Role/RoleForm";
 import UserTable from "../pages/User/UserTable";
-import UserForm from "../pages/User/UserForm";
+
 
 // settings
 import Settings from "../pages/Settings/index";
@@ -54,34 +45,22 @@ const SiteRoute = () => {
     return (
       <MainLayout>
         <Routes>  
-       
-
-          <Route path="/" element={<AllFiles/>} />        
-          <Route path="/sharewithme" element={<ShareWithMe/>} />
-          <Route path="/recylebin" element={<RecyleBin/>} />
-          <Route path="/roletable" element={<RoleTable/>} />
-          <Route path="/roleform" element={<RoleForm />} />
-          <Route path="/usertable" element={<UserTable />} />
-          <Route path="/userform" element={<UserForm/>} />
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Home />} />
-          <Route path="/pdftable" element={<Pdftable/>} />
-          <Route path="/newform" element={<Pdfform/>} />
-          <Route path="/pdfcal" element={<PdfCalendar/>} />
           <Route path="/signup" element={<Signup/>} />
           <Route path="/SmartLogin" element={<SmartLogin/>} />
 
           {/* all Files */ }
           <Route  path="/all-files" element={<AllFiles />}/>
-
+          <Route path="/share-with-me" element={<ShareWithMe/>} />
+          <Route path="/recycle-bin" element={<RecyleBin/>} /> 
 
           {/* Settings */}
           <Route  path="/settings" element={<Settings/>}/>
-          <Route  path="/email" element={<EmailSettings/>}/>
-          <Route  path="/file-settings" element={<FileSettings/>}/>
-          <Route  path="/login-settings" element={<LoginSettings/>}/>
-          <Route  path="/remote-server-settings" element={<RemoteServerSettings/>}/>
-          <Route  path="/site-settings" element={<SiteSettings/>}/>
+          {/* users and roles */}
+          <Route path="/roles" element={<RoleTable/>} />
+          <Route path="/users" element={<UserTable />} />
+         
         </Routes>
       </MainLayout>
     );

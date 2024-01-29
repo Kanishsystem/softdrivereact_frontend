1045f2047f60b487163341aa39ab9dd3a0cec4cd
kalkieswaran <155619180+kalkieswaran@users.noreply.github.com>
2024-01-29 14:47:13 +0530

    update

--- src/pages/AllFiles/AllFiles.jsx
+++ src/pages/AllFiles/AllFiles.jsx
@@ -1,56 +1,243 @@
 import React from 'react'
+import './AllFile.css';
+import { MAIN_PIC } from '../../services/ImageService';
+import StorageOverview from './StorageOverview';
 
 const AllFiles = () => {
+
+
+    const items = [
+        { img: { MAIN_PIC }, title: "Media report-Nov 2023" },
+        { img: { MAIN_PIC }, title: "Media report-Nov 2023" },
+        { img: { MAIN_PIC }, title: "Media report-Nov 2023" },
+        { img: { MAIN_PIC }, title: "Media report-Nov 2023" },
+    ]
+
     return (
-        <div>
-            <div className="columns ">
-                <div className="column is-12 has-background-light is-flex m-4">
-                    <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link is-size-5">
-                            <strong> My porta</strong>
-                        </a>
-                        <div className="navbar-dropdown">
-                            <a className="navbar-item">
-                                About
-                            </a>
-                            <a className="navbar-item">
-                                Jobs
-                            </a>
-                            <a className="navbar-item">
-                                Contact
-                            </a>
-                        </div>
-                    </div>
-                    <div className="navbar-item has-dropdown is-hoverable ">
-                        <a className="navbar-link">
-                            Brand Guideline
-                        </a>
-
-                        <div className="navbar-dropdown">
-                            <a className="navbar-item">
-                                About
-                            </a>
-                            <a className="navbar-item">
-                                Jobs
-                            </a>
-                            <a className="navbar-item">
-                                Contact
-                            </a>
-                        </div>
-                    </div>
-                    <div className='column is-flex is-justify-content-end'>
-                        <div className='listicon m-2 '>
-                            <i class="fa-solid fa-bars"></i>
-                        </div>
-                        <div className='alerticon m-2'>
-                            <i className="fa-solid fa-circle-exclamation"></i>
+        <div className='AllFiles-Frampage'>
+            <div className='columns'>
+                <div className="column is-8">
+                    <div className="columns ">
+                        <div className="column is-12 has-background-light is-flex ">
+                            <div class="navbar-item has-dropdown is-hoverable">
+                                <a class="navbar-link is-size-5">
+                                    My Drive
+                                </a>
+                                <div class="navbar-dropdown">
+                                    <a class="navbar-item">
+                                        About
+                                    </a>
+                                    <a class="navbar-item">
+                                        Jobs
+                                    </a>
+                                    <a class="navbar-item">
+                                        Contact
+                                    </a>
+                                </div>
+                            </div>
+                            <div className='column is-flex is-justify-content-end'>
+                                <div className='listicon p-2 '>
+                                    <i class="fa fa-bars" aria-hidden="true"></i>
+                                </div>
+                                <div className='alerticon p-2'>
+                                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
+                    <div className="columns  ">
+                        <div className="column is-12 box is-flex">
+                            <div className="AllFiles-topnav  pt-2">
+                                <div className=" file is-info">
+                                    <label>
+                                        <input className="file-input" type="file" name="resume" />
+                                        <span className="file-cta">
+                                            <span className="file-icon">
+                                                <i className="fa fa-upload" aria-hidden="true"></i>
+                                            </span>
+                                            <span className="file-label">
+                                                upload
+                                            </span>
 
+                                            <i className="fa fa-chevron-circle-down pl-4 " aria-hidden="true"></i>
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div className="dropdown is-hoverable ml-4">
+                                <div className="dropdown-trigger pt-2">
+                                    <button className="buttonx  " aria-haspopup="true" aria-controls="dropdown-menu4">
+                                        <span><i className="fa fa-plus" aria-hidden="true"></i> Create now </span>
+                                        <span className="icon is-small">
+                                            <i className="fa fa-chevron-circle-down pl-2" aria-hidden="true"></i>
+                                        </span>
+                                    </button>
+                                </div>
+                                <div className="dropdown-menu" id="dropdown-menu4" role="menu">
+                                    <div className="dropdown-content">
+                                        <div className="dropdown-item">
+                                            <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <span><i className="fa-solid fa-ellipsis-vertical pl-6 pt-3 is-size-4 "></i></span>
+
+                            <div className="column is-flex is-justify-content-end">
+                                <div className="dropdown is-hoverable ">
+                                    <div className="dropdown-trigger">
+                                        <button className="button-sort p-2 " aria-haspopup="true" aria-controls="dropdown-menu4">
+                                            <i className="fa fa-angle-double-down pr-2" aria-hidden="true"></i>
+                                            Sart :Last Moditiod
+                                            <i className="fa-solid fa-arrow-down-long pl-2"></i><span />
+                                            <span className="icon is-small">
+                                                <i className="fa fa-long-arrow-down" aria-hidden="true"></i>
+                                            </span>
+                                        </button>
+                                    </div>
+                                    <div className="dropdown-menu" id="dropdown-menu4" role="menu">
+                                        <div className="dropdown-content">
+                                            <div className="dropdown-item">
+                                                <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div className="columns ">
+                        <div className="column is-12 is-size-5 p-4 box">
+                            Recommend
+                        </div>
+                    </div>
+                    <div className="columns ">
+                        <div className="column is-12 box is-flex">
+                            {items.map((item) =>
+                                <div className="column is-4 ">
+                                    <div className="card ">
+                                        <div className="card-content">
+                                            <p className="title">
+                                                <img src={MAIN_PIC} alt="" height="250" width="300" />
+                                            </p>
+                                        </div>
+                                        <footer className="card-footer p-3">
+                                            <i className="fa fa-address-card p-1 is-size-5" aria-hidden="true"></i>
+                                            {item.title}
+                                        </footer>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="columns ">
+                        <div className="column is-12 is-size-5 p-4 box">
+                            Folders
+                        </div>
+                    </div>
+                    <div className="columns ">
+                        <div className="column box is-12 ">
+                            <div className="AllFile-Files-Document ">
+
+                                <div className="column is-4">
+                                    <div className="card p-4  is-flex is-justify-content-space-between">
+                                        <i className="fa fa-file-code-o  " aria-hidden="true"></i>
+                                        Work Documents
+                                        <i className="fa fa-ellipsis-v  " aria-hidden="true"></i>
+                                    </div>
+
+                                </div>
+
+                                <div className="column is-4">
+                                    <div className="card p-4  is-flex is-justify-content-space-between">
+                                        <i className="fa fa-file-code-o  " aria-hidden="true"></i>
+                                        Promotion Photosh
+                                        <i className="fa fa-ellipsis-v  " aria-hidden="true"></i>
+                                    </div>
+
+                                </div>
+
+                                <div className="column is-4">
+                                    <div className="card p-4  is-flex is-justify-content-space-between">
+                                        <i className="fa fa-file-code-o  " aria-hidden="true"></i>
+                                        Intemship Program
+                                        <i className="fa fa-ellipsis-v  " aria-hidden="true"></i>
+                                    </div>
+                                </div>
+
+                                <div className="column is-4">
+                                    <div className="card  p-4  is-flex is-justify-content-space-between ">
+                                        <i className="fa fa-file-code-o  " aria-hidden="true"></i>
+                                        Marketing Media
+                                        <i className="fa fa-ellipsis-v  " aria-hidden="true"></i>
+
+                                    </div>
+
+                                </div>
+
+                                <div className="column is-4">
+                                    <div className="card  p-4  is-flex is-justify-content-space-between">
+                                        <i className="fa fa-file-code-o  " aria-hidden="true"></i>
+                                        Brand Guldelne
+                                        <i className="fa fa-ellipsis-v  " aria-hidden="true"></i>
+                                    </div>
+
+                                </div>
+
+                                <div className="column is-4">
+                                    <div className="card p-4  is-flex is-justify-content-space-between">
+                                        <i className="fa fa-file-code-o  " aria-hidden="true"></i>
+                                        Financial Reports
+                                        <i className="fa fa-ellipsis-v  " aria-hidden="true"></i>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="columns ">
+                        <div className="column is-12 is-size-5 p-4 box">
+                            Files
+                        </div>
+                    </div>
+                    <div className="columns ">
+                        <div className="column is-12 box is-flex">
+
+                            {items.map((item) =>
+                                <div className="column is-4 ">
+                                    <div className="card ">
+                                        <div className="card-content">
+                                            <p className="title">
+                                                <img src={MAIN_PIC} alt="" height="250" width="300" />
+                                            </p>
+                                        </div>
+                                        <footer className="card-footer p-3">
+                                            <i className="fa fa-address-card p-1 is-size-5" aria-hidden="true"></i>
+                                            {item.title}
+                                        </footer>
+                                    </div>
+                                </div>
+                            )}
+
+                        </div>
+                    </div>
+                </div>
+                <div className='column is-3'>
+                    <StorageOverview />
                 </div>
             </div>
+
+
+
+
         </div>
+
     )
 }
 
--- src/pages/AllFiles/StorageOverview.jsx
+++ src/pages/AllFiles/StorageOverview.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
-
+import { MAIN_PIC } from '../../services/ImageService';
 const StorageOverview = () => {
+
+
+  
   return (
-    <div>StorageOverview</div>
+    <div className='StorageOverview '>
+      <div className="columns ">
+        <div className="column is-flex is-justify-content-space-between box m-1 ">
+
+          <div className='column'>
+            <i className="fa fa-arrow-right " aria-hidden="true"></i>
+            <i className="fa fa-arrows pl-3" aria-hidden="true"></i>
+          </div>
+          <div className='column'>
+            <i className="fa fa-ellipsis-v  " aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='columns'>
+        <div className='column box ml-1'>
+          Storage
+          <div className='column ml-1'>
+            <img className="StorageimageOverview" src={MAIN_PIC} alt="" height="150" width="200" />
+          </div>
+        </div>
+      </div>
+      <div className='columns'>
+        <div className='column box ml-1'>
+          <div className="tabs  ">
+            <ul>
+              <li className="is-active"><a>Details</a></li>
+              <li><a>Activities</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className='columns'>
+        <div className='column box is-flex is-justify-content-space-between ml-1'>
+
+          <div className='column'>
+            <img className="StorageimageOverview p-2" src={MAIN_PIC} alt="" height="50" width="50" />
+            Document 223 files
+          </div>
+          <span>
+            760GB
+          </span>
+        </div>
+
+
+
+
+
+      </div>
+
+
+    </div >
   )
 }
 

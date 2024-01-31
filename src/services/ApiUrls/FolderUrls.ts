a3315341e320e365937c7dbb2528ceea1dcbe8e4
Janarthanan <139687402+jana050@users.noreply.github.com>
2024-01-31 10:21:58 +0530

    UPDATE

---
+++ src/services/ApiUrls/FolderUrls.ts
@@ -0,0 +1,9 @@
+const FOLDER_API_URLS = {
+    get_all: 'user/get_all',
+    get_one: 'user/get_one',
+    insert: 'user/insert',
+    update: 'user/update',
+    delete_one:'user/delete_one'
+};
+
+export default FOLDER_API_URLS;

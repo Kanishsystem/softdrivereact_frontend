f67897b0dd977bff2287bd72a574b2189bdd7fbd
kalkieswaran <155619180+kalkieswaran@users.noreply.github.com>
2024-01-31 15:54:11 +0530

    new update

--- src/services/ApiUrls/FolderUrls.ts
+++
@@ -1,9 +0,0 @@
-const FOLDER_API_URLS = {
-    get_all: 'user/get_all',
-    get_one: 'user/get_one',
-    insert: 'user/insert',
-    update: 'user/update',
-    delete_one:'user/delete_one'
-};
-
-export default FOLDER_API_URLS;

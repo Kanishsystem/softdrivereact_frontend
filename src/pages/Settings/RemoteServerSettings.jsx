eb036ff5a0fd8aaa9d2652ff94d80e7e882d3716
kalkieswaran <155619180+kalkieswaran@users.noreply.github.com>
2024-02-02 12:16:22 +0530

    new  changes

--- src/pages/Settings/RemoteServerSettings.jsx
+++ src/pages/Settings/RemoteServerSettings.jsx
@@ -1,70 +1,43 @@
 import React from 'react'
 import { SmartSoftInput } from 'soft_digi';
-
 const RemoteServerSettings = () => {
+
+  const remortes = [
+    { arowup: <i class="fa fa-line-chart p-2" aria-hidden="true"></i>, arowright: <i class="fa fa-share p-2" aria-hidden="true"></i>, trashbin: <i class="fa fa-trash p-2 has-text-danger" aria-hidden="true"></i> },
+    { arowup: <i class="fa fa-line-chart p-2" aria-hidden="true"></i>, arowright: <i class="fa fa-share p-2" aria-hidden="true"></i>, trashbin: <i class="fa fa-trash p-2 has-text-danger" aria-hidden="true"></i> },
+    { arowup: <i class="fa fa-line-chart p-2" aria-hidden="true"></i>, arowright: <i class="fa fa-share p-2" aria-hidden="true"></i>, trashbin: <i class="fa fa-trash p-2 has-text-danger" aria-hidden="true"></i> },
+  ]
   return (
-    
-    <div className="page-container1"> 
-   <div className="page-body is-flex is-flex-direction-row">
-     <div className="side-bar1 is-flex is-flex-direction-row">
-      <div className="part-container1">
-      <div className="icon-container1 is-flex is-flex-direction-row">
-      <i class="fa fa-line-chart" aria-hidden="true"></i>
-      <i class="fa fa-share" aria-hidden="true"></i>
-      <i class="fa fa-square-o" aria-hidden="true"></i>
-       </div>
-       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjjGm1SHuZZCBX1vmKw3-YAH1uJWaVAww9M0SQf4n_qnHhcIKgyQBIh27_uRLkpIJYWHs&usqp=CAU" className="logo"/>
-       <div className="icon-container2">
-       <i class="fa fa-eyedropper" aria-hidden="true"></i>
-       
-       </div>
+    <div className='RemoteServerSettings'>
+      <div className='columns  is-multiline  '>
+        <div className='column  is-multiline  is-flex card is-8'>
+          {remortes.map((item) =>
+            <div className='column  is-multiline  card is-4 '>
+              <div className='is-flex is-justify-content-end'>
+                {item.arowup}
+                {item.arowright}
+                {item.trashbin}
+              </div>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjjGm1SHuZZCBX1vmKw3-YAH1uJWaVAww9M0SQf4n_qnHhcIKgyQBIh27_uRLkpIJYWHs&usqp=CAU" className="logo" />
+              <i class="fa fa-eyedropper is-flex is-justify-content-end p-2" aria-hidden="true"></i>
+            </div>
+          )}
+        </div>
+        <div className='column  card is-4'>
+          <div className='card p-5'>
 
+            <SmartSoftInput label="Server Url" className="box-model" id="boxmodel" />
+            <SmartSoftInput label="Server Way " />
+            <SmartSoftInput label="Server file" />
+
+            <div className="btnproperty is-flex is-justify-content-end">
+              <span className='pr-6 pt-3'><a href='##'>Test</a></span>
+              <span><button class="button is-link">Add servar</button></span>
+            </div>
+          </div>
+        </div>
       </div>
-
-       </div>
-       <div className="part-container1">
-        <div className="icon-container1 is-flex is-flex-direction-row">
-        <i class="fa fa-line-chart" aria-hidden="true"></i>
-        <i class="fa fa-share" aria-hidden="true"></i>
-        <i class="fa fa-square-o" aria-hidden="true"></i>
-         </div>
-       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjjGm1SHuZZCBX1vmKw3-YAH1uJWaVAww9M0SQf4n_qnHhcIKgyQBIh27_uRLkpIJYWHs&usqp=CAU" className="logo"/> 
-       <div className="icon-container2">
-       <i class="fa fa-eyedropper" aria-hidden="true"></i>
-       </div>
-       
-       </div>
-       
-      
-       <div className="icon-container3">
-       <i class="fa fa-th" aria-hidden="true"></i>
-       </div> 
-       <div className="part-container2">
-       {/* <p className="Server-Url">Server Url
-       <input type="text" className="box1"/>
-        </p> */}
-
-        {/* <p className="Server-Way">Server Way
-       <input type="text" className="box1"/>
-        </p> */}
-         <SmartSoftInput  label="Server Url" className="box-model" id="boxmodel"/> 
-        <SmartSoftInput  label="Server Way"/> 
-        
-        <SmartSoftInput  label="Server file"/> 
-       
-      <div className="buuton-block is-flex is-flex-direction-row">
-       <button className="Next">Next</button>
-       <button className="Added">Added File</button>
-       </div>
-
-       </div> 
-      
-   </div>  
-
-</div>
-
-);
- 
+    </div>
+  );
 }
-
 export default RemoteServerSettings
